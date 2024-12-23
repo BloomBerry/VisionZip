b06c903da155ed249be9dc4aaa0628c99899e07b
BloomBerry <jyjang1090@gmail.com>
2024-12-23 23:38:28 +0900

    add self attention bug

--- visionzip/llava_arch.py
+++ visionzip/llava_arch.py
@@ -273,6 +273,8 @@ def prepare_inputs_labels_for_multimodal_withdino_visionzip(
 ):
     vision_tower = self.get_vision_tower()
     if vision_tower is None or images is None or input_ids.shape[1] == 1:
+        if past_key_values is not None and vision_tower is not None and images is not None and input_ids.shape[1] == 1:
+            attention_mask = torch.ones((attention_mask.shape[0], past_key_values[-1][-1].shape[-2] + 1), dtype=attention_mask.dtype, device=attention_mask.device)
         return input_ids, position_ids, attention_mask, past_key_values, None, labels
 
     if type(images) is list or images.ndim == 5:
